d6b32e46ee3ac30eb400d5c6bdcc9bd851cfe834
Patrick Hausmann <patrick.hausmann@covimo.de>
2017-07-21 15:52:06 +0200

    update parameter

--- R/effisr-search.R
+++ R/effisr-search.R
@@ -1,27 +1,24 @@
-#' Search EFFIS
-#'
+#' Search the European Forest Fire Information System 'EFFIS'
 #'
 #' @export
 #'
-#' @param country (character) Country name, a 2 letter code
-#' @param countryful (character) Country name, long name
+#' @param country (character) ISO country code
 #' @param province (character) Province name
-#' @param commune (character) Commune
 #' @param limit (integer) record limit
-#' @param critech (character) one of 'YES'
-#' @param firedate (character) YYYy-MM-DD
+#' @param firedate (character) YYYY-MM-DD
 #' @param area_ha (integer) size
+#' @param ba_class (character) one of 'ALL', '30DAYS' or '07DAYS'
 #' @param page (integer) Page number
 #' @param ... Curl options passed on to \code{\link[httr]{GET}}
 
-eff_search <- function(country = NULL, countryful = NULL, province = NULL,
-                    commune = NULL, limit = NULL, critech = NULL,
-                    firedate = NULL, area_ha = NULL, page = NULL, ...)
+eff_search <- function(country = NULL, province = NULL,
+                       limit = NULL, firedate = NULL,
+                       area_ha = NULL, ba_class = NULL,
+                       page = NULL, ...)
 {
-  args <- cacomp(list(country = country, countryful = countryful,
-                      province = province, commune = commune,
-                      limit = limit, critech = critech,
-                      firedate = firedate, area_ha = area_ha,
+  args <- cacomp(list(country = country, province = province,
+                      limit = limit, firedate = firedate,
+                      area_ha = area_ha, ba_class = ba_class,
                       fmt = "json"))
   res <- ca_GET(cabase(), args, ...)
   list(docs = parse_facet(res)$result,
